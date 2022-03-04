38733b84c17e9a099e95b90a58efda1dde22ea01
Nick Truby <nick@truby.io>
2022-03-04 16:32:48 +0000

    added _document.tsx file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,25 +8,6 @@ const Index: NextPage = () => {
     <>
       <Head>
         <title>Midas Leasing</title>
-        <meta
-          name="description"
-          content="Midas Leasing provide loans, finance contracts and leasing for Companies across the UK - Based in Tamworth, Staffordshire"
-        />
-        <meta
-          name="keywords"
-          content="Tamworth, Staffordshire, Birmingham, Leasing, Business, Loans, Finance, Finance Contracts, Business Loans, Business Leasing, UK"
-        />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
-        <meta property="og:site_name" content="Midas Leasing" />
-        <meta
-          property="og:image"
-          content="http://www.midasleasing.co.uk/assets/images/512x512"
-        />
-        <meta property="og:url" content="http://www.midasleasing.co.uk/" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
